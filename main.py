777cb5bd7d13eef873dbc527cb8d78ec8715d95f
detailyang <detailyang@gmail.com>
2016-11-14 10:31:54 +0800

    add mysql port

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ from pymysql import cursors
 env_get = environ.get
 CAS_CRL_ENV = env_get('CAS_CRL_ENV', 'DEV')
 CAS_CRL_KEY = env_get('CAS_CRL_KEY')
-CAS_CRL_CRT = env_get('CAS_CRL_CRT')
 CAS_CRL_KEY_PASSWD = env_get('CAS_CRL_KEY_PASSWD')
+CAS_CRL_CRT = env_get('CAS_CRL_CRT')
 CAS_CRL_URL = env_get('CAS_CRL_URL')
-CAS_CRL_MYSQL_USER = env_get('CAS_CRL_MYSQL_USER')
 CAS_CRL_MYSQL_HOST = env_get('CAS_CRL_MYSQL_HOST')
+CAS_CRL_MYSQL_PORT = env_get('CAS_CRL_MYSQL_PORT', '3306')
+CAS_CRL_MYSQL_USER = env_get('CAS_CRL_MYSQL_USER')
 CAS_CRL_MYSQL_PASSWD = env_get('CAS_CRL_MYSQL_PASSWD')
 CAS_CRL_MYSQL_DB = env_get('CAS_CRL_MYSQL_DB')
 CAS_CRL_MYSQL_CHARSET = env_get('CAS_CRL_MYSQL_CHARSET', 'utf8mb4')
@@ -36,6 +37,7 @@ def crl():
     # Connect to the database
     try:
         connection = pymysql.connect(host=CAS_CRL_MYSQL_HOST,
+           port=CAS_CRL_MYSQL_PORT,
            user=CAS_CRL_MYSQL_USER,
            password=CAS_CRL_MYSQL_PASSWD,
            db=CAS_CRL_MYSQL_DB,
